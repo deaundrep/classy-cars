1d88b486f55be45ca35e1f255ab78ca755c51cb2
DeAundre Prescott <deaundre.prescott@codeimmersives.com>
2020-04-12 23:53:09 -0400

    done

--- main.js
+++ main.js
@@ -1,11 +1,49 @@
 class Vehicle {
+  constructor (name, wheelCount, maxSpeed, speed = 0){
+    this.name = name,
+    this.wheelCount = wheelCount,
+    this.maxSpeed = maxSpeed,
+    this.speed = speed;
+  }
+  accelerate(){
+    if ((this.maxSpeed - this.speed) < (this.maxSpeed/3)) {
+      this.speed = this.maxSpeed;
+    } else {
+      this.speed = this.speed + this.maxSpeed/3
+    }
+  }
 }
 
-class Car {
+class Car extends Vehicle {
+  constructor(name,maxSpeed, speed){
+    super(name,4,maxSpeed, speed)
+}
+drive(){
+  return `Vroom. `;
+}
 }
 
-class Ferrari {
+
+class Ferrari extends Car{
+  constructor() {
+    super('Ferrari', 180);
+  }
+  drive() {
+    return super.drive() + "I'm a Ferrari!";
+  }
+
+accelerate() {
+  if ((this.maxSpeed - this.speed) < (this.maxSpeed/2)){
+    this.speed = this.maxSpeed;
+  } else {
+    this.speed = this.speed + this.maxSpeed/2
+  }
+  
+  }
 }
+
+
+
 
 
 module.exports = {
